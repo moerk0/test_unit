1db2eae4cf0426f0606ae87152fd7ef6b6276814
moerk0 <ishmael_goony@mailbox.org>
2022-04-25 12:57:36 +0200

    v

--- encoder.py
+++ encoder.py
@@ -2,136 +2,142 @@ import logging as log
 from test_data import TestData, ReturnCode
 from excelHandler import Excel
 
+
 class Encoder:
-    def __init__(self,d:'list[TestData]', lang) -> None:
+    def __init__(self, d: "list[TestData]", lang) -> None:
         self.d = d
         self.lang = str(lang)
         self.filename = f'./data/braille_{self.lang[0:3].lower().replace("-","")}.h'
 
-
     def sort_data(self) -> None:
-        self.d.sort(key= lambda x: getattr(x, 'num'))
+        self.d.sort(key=lambda x: getattr(x, "num"))
 
     def header(self) -> str:
         s = "\n"
-        special_char_map = {ord('Ä'):'A', ord('Ü'):'U', ord('Ö'):'O'}
-        guardname = self.filename[-13:-2].upper().replace('-','').translate(special_char_map).replace('/','')
-       
-        
-
-        ifguard =(
-                f"#ifndef __{guardname}_H__",
-                f"#define __{guardname}_H__",
-                2*'\n'
+        special_char_map = {ord("Ä"): "A", ord("Ü"): "U", ord("Ö"): "O"}
+        guardname = (
+            self.filename[-13:-2]
+            .upper()
+            .replace("-", "")
+            .translate(special_char_map)
+            .replace("/", "")
         )
 
-        include = ('#include "keylayouts.h"', 2*'\n')
-        
-        declaration = ( "// Erich Schmids 8 Keys Braille",
+        ifguard = (f"#ifndef __{guardname}_H__", f"#define __{guardname}_H__", 2 * "\n")
+
+        include = (
+            "#include <stdio.h>",
+            "#include <string.h>",
+            "#include <stdlib.h>",
+            "#include <stdint.h>",
+            '#include "keylayouts.h"',
+            2 * "\n",
+        )
+
+        declaration = (
+            "// Erich Schmids 8 Keys Braille",
             "const static uint16_t chord_id_keycode[256][4] = {",
-            "//            {char, char_modifier, deadkey, deadkey_modifier}"
+            "//            {char, char_modifier, deadkey, deadkey_modifier}",
         )
         head = ifguard + include + declaration
         return s.join(head)
-    
-    def array(self,key,mod,dead,dmod) -> str:
+
+    def array(self, key, mod, dead, dmod) -> str:
         if key == None:
             key = 0
 
         elif key == "GRAVE" or key == "SCANCODE_GRAVE":
-            key ='SCANCODE_GRAVE'
-        
-        
+            key = "SCANCODE_GRAVE"
 
         else:
-            key = 'KEY_' + key
+            key = "KEY_" + key
 
         if mod == None:
             mod = 0
-        else: 
-            mod = 'MODIFIERKEY_' + mod
+        else:
+            mod = "MODIFIERKEY_" + mod
 
         if dead == None:
             dead = 0
         elif dead == "GRAVE" or dead == "SCANCODE_GRAVE":
-            dead = 'SCANCODE_GRAVE'
-        else: 
-            dead = 'DEADKEY_' + dead
+            dead = "SCANCODE_GRAVE"
+        else:
+            dead = "DEADKEY_" + dead
 
         if dmod == None:
             dmod = 0
-        else: 
-            dmod = 'MODIFIERKEY_' + dmod
-
+        else:
+            dmod = "MODIFIERKEY_" + dmod
 
         s = f"{key}, {mod}, {dead}, {dmod}"
-        return '{' + s + '},'
+        return "{" + s + "},"
 
-    def num(self, num)-> str:
+    def num(self, num) -> str:
         s = f"/* {num}  */"
         return s
 
     def braille_cha(self, brai, cha) -> str:
         s = f"/* '{cha}' dots-{brai} */"
         return s
-    
-    def null_line(self, num)-> str:
-        s = f"/* {num}  */\t"+"{0, 0, 0, 0},"+ 59*' ' +"// 0"
+
+    def null_line(self, num) -> str:
+        s = f"/* {num}  */\t" + "{0, 0, 0, 0}," + 59 * " " + "// 0"
         return s
 
-    def end_of_file(self)->str:
-        s = "};" + 2*'\n' + '#endif'
+    def end_of_file(self) -> str:
+        s = "};" + 2 * "\n" + "#endif"
         return s
 
-    def tab_handler(self,len)-> str:
+    def tab_handler(self, len) -> str:
         max_space = 72
         space = max_space - len
-        return space*' '
+        return space * " "
 
     def run(self):
         self.sort_data()
 
-        with open(self.filename, 'w') as file:                # a = append, w = (over)write
+        with open(self.filename, "w") as file:  # a = append, w = (over)write
             print(self.header(), file=file)
             idx = 0
             cnt = 0
             prev_num = -1
             while idx < len(self.d):
-                data = self.d[idx]    
+                data = self.d[idx]
                 if prev_num == data.num or cnt > data.num:
-                    log.error(f"{prev_num} exists twice !!!! Not good. You almost buffer overflowed")
+                    log.error(
+                        f"{prev_num} exists twice !!!! Not good. You almost buffer overflowed"
+                    )
                     break
-                
+
                 # if data is shitty
-                elif data.color_key >= ReturnCode.YELLOW or data.color_mod >= ReturnCode.YELLOW or data.color_dmo >= ReturnCode.YELLOW:
+                elif (
+                    data.color_key >= ReturnCode.YELLOW
+                    or data.color_mod >= ReturnCode.YELLOW
+                    or data.color_dmo >= ReturnCode.YELLOW
+                ):
                     print(self.null_line(cnt), file=file)
                     idx += 1
 
                 elif cnt == data.num:
-                    print(self.num(data.num), file=file, end='\t')
+                    print(self.num(data.num), file=file, end="\t")
                     arr = self.array(data.key, data.mod, data.dead, data.dmod)
                     print(arr, file=file, end=self.tab_handler(len(arr)))
                     print(self.braille_cha(data.brai, data.cha), file=file)
                     prev_num = data.num
-                    idx +=1
+                    idx += 1
                 else:
                     print(self.null_line(cnt), file=file)
 
-                cnt+=1
-                
+                cnt += 1
+
             log.info(f"{cnt} Rows encoded")
             print(self.end_of_file(), file=file)
 
 
-
-#numbers= [1,3,5,7,9,10,11,12,13,15,17,19,20]
-
+# numbers= [1,3,5,7,9,10,11,12,13,15,17,19,20]
 
 
 # dat= [TestData(0,n,randint(100,200000),'a','A',' ',' ',' ') for n in numbers]
 # encoder = Encoder(dat,'türkisch')
 # encoder.sort_data()
 # encoder.encode()
-
-
-
--- test_data.py
+++ test_data.py
@@ -1,32 +1,47 @@
-from ast import Return
 from dataclasses import dataclass
 import logging as log
 from enum import IntEnum, Enum, auto
 
 log.basicConfig(level=log.INFO, format="%(levelname)s:%(message)s")
 
-@dataclass
+
 class ReturnCode(IntEnum):
-    """"These Codes color the output file and determine if the giben data is faulty"""
+    """ "These Codes color the output file and determine if the giben data is faulty"""
 
     BLANK = 0
     GREEN = 1
     YELLOW = 2
     RED = 3
 
+
 class OutLevel(Enum):
     INITIAL = auto()
     TEST = auto()
 
 
+class DataIndex(Enum):
+    ROW = auto()
+    NUM = auto()
+    BRAI = auto()
 
-all_nums = [] # validator occupys global namespace here
+    CHA = auto()
+    KEY = auto()
+    MOD = auto()
+    DEAD = auto()
+    DMOD = auto()
+
+    REC = auto()
+    PASSED = auto()
+
+
+all_nums = []  # validator occupys global namespace here
+
 
 @dataclass
 class TestData:
     """
     Each one object holds one Row of ExcelTable and it holds the Color coding of the result file
-    Initially TestData requires 4 Inputs: 
+    Initially TestData requires 4 Inputs:
     The Bin Num to send, The expected Char, The name of the key and The modifier Key.
 
     Recieved and Passed are Values that the Test_unit Program fills. They are not mandatory for the Encoder,
@@ -37,7 +52,7 @@ class TestData:
     OR
     The Test is passed
 
-    Yellow means there was a mistake that influences the Output of the .h -File 
+    Yellow means there was a mistake that influences the Output of the .h -File
     OR
     for the received Column it means that no data arrieved( receivied = None) during runtime of the test
 
@@ -46,23 +61,23 @@ class TestData:
     For the passed Column it means that the expected char and the recieved Char are not equal.
 
     """
-    
-    row : int
+
+    row: int
 
     num: int
     brai: str
     cha: str
-    
+
     key: str
     mod: str
 
     dead: str
-    dmod : str
+    dmod: str
 
     received = None
-    passed: bool =  False
-    
-    #Color Codes: 0 = No Color, 1 = Green, 2 = Yellow, 3 = Red
+    passed: bool = False
+
+    # Color Codes: 0 = No Color, 1 = Green, 2 = Yellow, 3 = Red
     color_num: ReturnCode = ReturnCode.BLANK
     color_cha: ReturnCode = ReturnCode.BLANK
     color_key: ReturnCode = ReturnCode.BLANK
@@ -72,191 +87,221 @@ class TestData:
     color_rec: ReturnCode = ReturnCode.BLANK
     color_pas: ReturnCode = ReturnCode.RED
 
-
     def checkASCII(self, arg, var_name) -> ReturnCode:
         if str(arg).isascii():
             code = ReturnCode.BLANK
         else:
-            log.warning(f'\tRow: {self.row} - {var_name} >> {arg} << is not ASCII')
+            log.warning(f"\tRow: {self.row} - {var_name} >> {arg} << is not ASCII")
             code = ReturnCode.YELLOW
         return code
 
-    def checkCase(self,arg, var_name)->"tuple[str, ReturnCode]":
+    def checkCase(self, arg, var_name) -> "tuple[str, ReturnCode]":
         if str(arg).isupper() == False:
             arg = arg.upper()
-            log.info(f'\tRow: {self.row} - {var_name} >> {arg} << is not upper. Corrected')
+            log.info(
+                f"\tRow: {self.row} - {var_name} >> {arg} << is not upper. Corrected"
+            )
             code = ReturnCode.GREEN
         else:
             code = ReturnCode.BLANK
         return arg, code
 
-    def checkSpace(self, arg, var_name)-> "tuple[str, ReturnCode]":
+    def checkSpace(self, arg, var_name) -> "tuple[str, ReturnCode]":
         if " " in arg:
             arg = arg.strip()
-            log.info(f'\tRow: {self.row} - {var_name} >> {arg} << unexpected Space character. Corrected')
+            log.info(
+                f"\tRow: {self.row} - {var_name} >> {arg} << unexpected Space character. Corrected"
+            )
             code = ReturnCode.GREEN
         else:
             code = ReturnCode.BLANK
         return arg, code
-            
 
-    def checkList(self, arg:str, l, var_name:str) -> "tuple[str,ReturnCode]":
-        
+    def checkList(self, arg: str, l, var_name: str) -> "tuple[str,ReturnCode]":
+
         if arg in l:
             return arg, ReturnCode.BLANK
-        
+
         else:
             if arg.replace(" ", "_") in l:
                 arg = arg.replace(" ", "_")
-                log.info(f'\tRow: {self.row} - {var_name} >> {arg} << had no underscore')
+                log.info(
+                    f"\tRow: {self.row} - {var_name} >> {arg} << had no underscore"
+                )
                 return arg, ReturnCode.GREEN
             else:
-                log.warning(f'Row: {self.row} - {var_name} >> {arg} << is not in Key list')
-                return arg, ReturnCode.YELLOW            
-               
-
-
+                log.warning(
+                    f"Row: {self.row} - {var_name} >> {arg} << is not in Key list"
+                )
+                return arg, ReturnCode.YELLOW
 
     def validate_num(self):
         if self.num in all_nums:
-            log.error(f'\tRow: {self.row} - Num >> {self.num} << Duplicate Entry!')
+            log.error(f"\tRow: {self.row} - Num >> {self.num} << Duplicate Entry!")
             self.color_num = ReturnCode.RED
         else:
             all_nums.append(self.num)
-        
- 
 
     def validate_cha(self):
         if self.cha is None:
             log.warning(f"Row: {self.row} - Cha is None")
             self.color_cha = ReturnCode.YELLOW
 
-        elif str(self.cha).isspace() == True:                        # if it is escaped char
+        elif str(self.cha).isspace() == True:  # if it is escaped char
             pass
 
         else:
             self.cha = str(self.cha)
             len_cha = len(self.cha)
-            
+
             if len_cha > 1:
                 self.cha = self.cha.strip()
-                    
+
                 try:
                     ord(self.cha)
-                    log.info(f"\tRow: {self.row} - Cha >> {self.cha} << had a space character. Removed")
+                    log.info(
+                        f"\tRow: {self.row} - Cha >> {self.cha} << had a space character. Removed"
+                    )
                     self.color_cha = ReturnCode.GREEN
                     self.cha = self.cha.strip()
                 except:
-                    log.error(f"\tRow: {self.row} - Cha >> {self.cha} << is not a single character!")
+                    log.error(
+                        f"\tRow: {self.row} - Cha >> {self.cha} << is not a single character!"
+                    )
                     self.color_cha = ReturnCode.RED
             else:
                 pass
-                
-                 
+
     def validate_key(self):
-        key_names = ['SPACE','TAB','ENTER','PLUS', 'MINUS','QUOTE','PERIOD', 'SEMICOLON', 'EQUAL','SLASH' 
-                    'LEFT_BRACE', 'RIGHT_BRACE','GRAVE', 'NON_US_BS','BACKSLASH','COMMA', 'SCANCODE_GRAVE'] 
-        
+        key_names = [
+            "SPACE",
+            "TAB",
+            "ENTER",
+            "PLUS",
+            "MINUS",
+            "QUOTE",
+            "PERIOD",
+            "SEMICOLON",
+            "EQUAL",
+            "SLASH" "LEFT_BRACE",
+            "RIGHT_BRACE",
+            "GRAVE",
+            "NON_US_BS",
+            "BACKSLASH",
+            "COMMA",
+            "SCANCODE_GRAVE",
+        ]
+
         if self.key is None:
             log.warning(f"Row: {self.row} - Key is None")
             self.color_key = ReturnCode.YELLOW
-        
 
-                                                                # Key is not a int Number
+            # Key is not a int Number
         else:
             self.key = str(self.key).strip()
             ret = self.checkSpace(self.key, "key")
             self.key = ret[0]
             self.color_key = ret[1]
 
-
-
-            if self.key.isalpha() and len(self.key)<=1:
-                ret = self.checkASCII(self.key, 'key')
+            if self.key.isalpha() and len(self.key) <= 1:
+                ret = self.checkASCII(self.key, "key")
                 if self.color_key < ret:
                     self.color_key = ret
-                    
+
                 ret = self.checkSpace(self.key, "key")
                 self.key = ret[0]
                 if self.color_key < ret[1]:
                     self.color_key = ret[1]
-                
 
             elif self.key.isdigit():
                 pass
             else:
-                ret = self.checkList(self.key,key_names, "key")
+                ret = self.checkList(self.key, key_names, "key")
                 self.key = ret[0]
                 if self.color_key < ret[1]:
                     self.color_key = ret[1]
-            
-            
-
-            
-   
-
 
     def validate_mod(self):
-        modifier = ['SHIFT', 'RIGHT_ALT', 'LEFT_ALT', 'CTRL']
-            
+        modifier = ["SHIFT", "RIGHT_ALT", "LEFT_ALT", "CTRL"]
+
         if self.mod is not None:
-            ret =  self.checkCase(self.mod, 'Mod')
-            self.mod =       ret[0]
+            ret = self.checkCase(self.mod, "Mod")
+            self.mod = ret[0]
             self.color_mod = ret[1]
-            
+
             ret = self.checkSpace(self.mod, "Mod")
-            self.mod =       ret[0]
+            self.mod = ret[0]
             if ret[1] >= self.color_mod:
                 self.color_mod = ret[1]
 
             ret = self.checkList(self.mod, modifier, "Mod")
-            self.mod =       ret[0]
+            self.mod = ret[0]
             if ret[1] >= self.color_mod:
                 self.color_mod = ret[1]
-    
+
     def validate_dmod(self):
-        modifier = ['SHIFT', 'RIGHT_ALT', 'LEFT_ALT', 'CTRL']
-            
+        modifier = ["SHIFT", "RIGHT_ALT", "LEFT_ALT", "CTRL"]
+
         if self.dmod is not None:
-            ret =  self.checkCase(self.dmod, "dmod")
-            self.dmod =       ret[0]
+            ret = self.checkCase(self.dmod, "dmod")
+            self.dmod = ret[0]
             self.color_dmo = ret[1]
-            
-            ret = self.checkSpace(self.dmod,"dmod")
-            self.dmod =       ret[0]
+
+            ret = self.checkSpace(self.dmod, "dmod")
+            self.dmod = ret[0]
             if ret[1] >= self.color_dmo:
                 self.color_dmo = ret[1]
 
-            if self.dmod not in modifier:    
-               
-                
+            if self.dmod not in modifier:
+
                 if self.dmod in modifier:
-                    log.info(f"\tRow: {self.row} - Mod >> {self.dmod} << had no underscore. Corrected")
+                    log.info(
+                        f"\tRow: {self.row} - Mod >> {self.dmod} << had no underscore. Corrected"
+                    )
                     self.color_dmod = ReturnCode.GREEN
                 else:
-                    log.warning(f"Row: {self.row} - Mod >> {self.dmod} << is unkonwn modifier")
+                    log.warning(
+                        f"Row: {self.row} - Mod >> {self.dmod} << is unkonwn modifier"
+                    )
                     self.color_dmo = ReturnCode.YELLOW
-                        
-                   
-       
+
     #    ret =  self.checkCase(self.dmod)
     #         self.mod =       ret[0]
     #         self.color_mod = ret[1]
 
     def validate_all(self):
-        l = [self.validate_num, self.validate_cha, self.validate_key, self.validate_mod, self.validate_dmod]
+        l = [
+            self.validate_num,
+            self.validate_cha,
+            self.validate_key,
+            self.validate_mod,
+            self.validate_dmod,
+        ]
         for call_func in l:
             call_func()
-        
 
-    def content(self, level : OutLevel):
+    def content(self, level: OutLevel):
 
         if level == OutLevel.INITIAL:
-            return {'Row':self.row,"Num": self.num, "Braille":self.brai,"Cha": self.cha,"Key":self.key, "Mod":self.mod, "Dead": self.dead, "Dmod": self.dmod}
+            return {
+                "Row": self.row,
+                "Num": self.num,
+                "Braille": self.brai,
+                "Cha": self.cha,
+                "Key": self.key,
+                "Mod": self.mod,
+                "Dead": self.dead,
+                "Dmod": self.dmod,
+            }
 
         elif level == OutLevel.TEST:
-            return {'Num': self.num, "cha": self.cha, "received": self.received, "passed": self.passed}
-    
+            return {
+                "Num": self.num,
+                "cha": self.cha,
+                "received": self.received,
+                "passed": self.passed,
+            }
+
 
 # d = TestData(1,1,1,'\r ','A','SHIFT',None,None)
 # d.validate_all()
